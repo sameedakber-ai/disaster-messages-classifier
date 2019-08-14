f1180b93c2a447ac2326c10a1b9f8d644a527451
Sameed Akber <sameedakber.ai@gmail.com>
2019-08-14 17:01:10 -0400

    feat: Save data to file

--- data/process_data.py
+++ data/process_data.py
@@ -3,26 +3,30 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-  messages = pd.read_csv(messages_filepath)
-  categories = pd.read_csv(categories_filepath)
-  df = messages.merge(categories, on='id', how='left')
-  return df
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = messages.merge(categories, on='id', how='left')
+    return df
 
 def clean_data(df):
-  categories = df.categories.str.split(';', expand=True)
+    categories = df.categories.str.split(';', expand=True)
 
-  row = categories.iloc[0,:]
-  category_colnames = row.apply(lambda x: x[:-2])
-  categories.columns = category_colnames
-  for column in categories.columns:
+    row = categories.iloc[0,:]
+    category_colnames = row.apply(lambda x: x[:-2])
+    categories.columns = category_colnames
+    for column in categories.columns:
     categories[column] = categories[column].str.split('-').str[1]
     categories[column] = pd.to_numeric(categories[column])
 
-  df = pd.concat([df.drop('categories', axis=1),categories], axis=1)
+    df = pd.concat([df.drop('categories', axis=1),categories], axis=1)
 
-  df = df[~df.duplicated()]
+    df = df[~df.duplicated()]
 
-  return df
+    return df
+
+def save_data(df, database_filename):
+    engine = create_engine('sqlite:///{}'.format(database_filename))
+    df.to_sql('categories', engine, index=False, if_exists='replace')
 
 
 def main():
