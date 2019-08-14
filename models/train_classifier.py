a32b25b61b9b6c12a514a8bd15e64025502f096a
Sameed Akber <sameedakber.ai@gmail.com>
2019-08-14 16:24:52 -0400

    feat: Import text processing and ML libraries

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,5 +1,35 @@
 import sys
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sqlalchemy import create_engine
+import re
+import nltk
+nltk.download(['stopwords', 'punkt', 'averaged_perceptron_tagger', 'wordnet'])
+from scipy import sparse
+from collections import defaultdict
 
+from nltk import pos_tag
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize, sent_tokenize
+
+from sklearn.pipeline import Pipeline,FeatureUnion
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.metrics import precision_recall_fscore_support
+from sklearn.model_selection import cross_val_score
+from sklearn.base import BaseEstimator, TransformerMixin
+
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.svm import LinearSVC
+from sklearn.linear_model import SGDClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.model_selection import GridSearchCV
 
 def load_data(database_filepath):
     pass
